6db66a9637728b80029d90cda3713b2eb8c8920d
alter <alter@alterChiu-PC>
2018-07-09 17:24:59 +0800

    20180709

--- Sobek_MapReduce/src/main/MapReduceMainPage.java
+++ Sobek_MapReduce/src/main/MapReduceMainPage.java
@@ -4,40 +4,41 @@ import java.io.IOException;
 
 import Ascii.TimeControl.SplitTimeCount;
 import Ascii.TimeControl.TotalTimeCount;
-public class main {
+
+public class MapReduceMainPage {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub;
+		// TODO Auto-generated method stub
 		SplitTimeCount splitTimeCount = new SplitTimeCount();
 		TotalTimeCount totalTimeCount = new TotalTimeCount();
 		InitializeFolder initialize = new InitializeFolder();
 		
-		System.out.println("======================= Initialize Folder =================");
+		System.out.println("========= Initialize Folder =================");
 		initialize.createBeforeSplitCount();
 
-		System.out.println("========================= Delicate Time Count ======================");
-		System.out.println("total");
+		System.out.println("======= Delicate Time Count ================");
+		System.out.print("total    ");
 		totalTimeCount.DelicateTotalTimeCount();
 		
-		System.out.println("======================== Set Split Size ====================");
+		System.out.println("========== Set Split Size ==================");
 		initialize.setSplitSize();
 		initialize.createAfterSplitCount();
 
-		System.out.println("======================== Split Time Count ====================");
+		System.out.println("========= Split Time Count ================");
 		System.out.println("split_ Horizantal");
 		splitTimeCount.setDelicateHorizantal();
 		
 		System.out.println("split_Straight");
 		splitTimeCount.setDelicateStraight();
 
-		System.out.println("====================== Rough Time Count ======================");
-		System.out.println("total");
+		System.out.println("======== Rough Time Count ================");
+		System.out.print("total    ");
 		totalTimeCount.RoughTotalTimeCount();
 
 		System.out.println("================== determine the ascii section =========");
 		System.out.println();
 		
+
 	}
 
-	
 }
